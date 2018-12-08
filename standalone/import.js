62e162b6f29479dd1851a11a13f18af3b4f17972
Johnny B <typngeek@aim.com>
2018-12-08 03:18:53 -0500

    Implemented standalone nodejs files
    Used for sending email and loading in seniors

---
+++ standalone/import.js
@@ -0,0 +1,12 @@
+var Student = require('../models/student');
+
+Student.create({
+    fname: '',
+    lname: '',
+    nameAsAppears: '',
+    email: '',
+    major: '',
+    major2: '',
+    minor: '',
+    quote: ''
+});
