0f83e93d8775eb5c0c9291fe714587cfdc4b9256
Johnny B <typngeek@aim.com>
2018-12-10 02:21:15 -0500

    Update import standalone file

--- standalone/import.js
+++ standalone/import.js
@@ -1,12 +1,29 @@
+const mongoose = require('../middlewares/mongoose');
 var Student = require('../models/student');
 
-Student.create({
-    fname: '',
-    lname: '',
-    nameAsAppears: '',
-    email: '',
-    major: '',
-    major2: '',
-    minor: '',
-    quote: ''
-});
+// load csv
+const fs = require('fs'); 
+const csv = require('csv-parser');
+
+fs.createReadStream(process.argv[2])
+.pipe(csv())
+.on('data', function(data){
+    try {
+        Student.create({
+            fname: data.First,
+            lname: data.Last,
+            nameAsAppears: data.First + ' ' + data.Last,
+            email: data.Email,
+            major: '',
+            major2: '',
+            minor: '',
+            quote: ''
+        });
+    }
+    catch(err) {
+        console.log(err.message)
+    }
+})
+.on('end',function(){
+    console.log('done')
+});  
