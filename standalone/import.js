eb86846272d00c579677ea7de7cb0f01d04436e5
Johnny B <typngeek@aim.com>
2018-12-11 12:58:14 -0500

    Edited import js standalone file

--- standalone/import.js
+++ standalone/import.js
@@ -1,29 +1,49 @@
+// Start dotenv
+const dotenv = require('dotenv');
+const result = dotenv.config();
+if (result.error) {
+    throw result.error;
+}
+
 const mongoose = require('../middlewares/mongoose');
 var Student = require('../models/student');
 
 // load csv
-const fs = require('fs'); 
+const fs = require('fs');
 const csv = require('csv-parser');
 
-fs.createReadStream(process.argv[2])
-.pipe(csv())
-.on('data', function(data){
-    try {
-        Student.create({
-            fname: data.First,
-            lname: data.Last,
-            nameAsAppears: data.First + ' ' + data.Last,
-            email: data.Email,
-            major: '',
-            major2: '',
-            minor: '',
-            quote: ''
-        });
-    }
-    catch(err) {
-        console.log(err.message)
-    }
-})
-.on('end',function(){
-    console.log('done')
-});  
+// fs.createReadStream(process.argv[2])
+// .pipe(csv())
+// .on('data', function(data){
+//     try {
+//         Student.create({
+//             fname: data.First,
+//             lname: data.Last,
+//             nameAsAppears: data.First + ' ' + data.Last,
+//             email: data.Email,
+//             major: '',
+//             major2: '',
+//             minor: '',
+//             quote: ''
+//         });
+//     }
+//     catch(err) {
+//         console.log(err.message)
+//     }
+// })
+// .on('end',function(){
+//     console.log('done')
+// });  
+
+Student.create({
+    fname: 'Jane',
+    lname: 'Doe',
+    nameAsAppears: 'Jane Doe',
+    email: 'doe@mit.edu',
+    major: '',
+    major2: '',
+    minor: '',
+    quote: ''
+}, () => {
+    process.exit();
+});
