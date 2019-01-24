bddb4f1ca6eb7b0abcf37a19699af9baee9273ed
Johnny B <typngeek@aim.com>
2019-01-24 17:24:54 +0200

    Fix email text

--- standalone/index.js
+++ standalone/index.js
@@ -85,7 +85,7 @@ function sendEmails(dryRun) {
                     preheader: "It's time to confirm your Technique 2019 yearbook entry.",
                     superheader: 'Hey ' + student.fname + ',',
                     header: "There's just one more step...",
-                    paragraph: "Thank you for taking your senior portrait! Now, it's time to enter your senior quote information and confirm your yearbook entry for Technique 2019. You can log in and confirm your biogrpahical information in less than 60 seconds through the student portal. If you do not confirm by the deadline of 02/01, then Technique will not be responsible for any inaccuracies in your senior bio. Happy holidays from MIT Technique!",
+                    paragraph: "Thank you for taking your senior portrait! Now, it's time to enter your senior quote information and confirm your yearbook entry for Technique 2019. You can log in and enter your biographical information in less than 60 seconds through the student portal. If you do not confirm by the deadline of 02/01, then Technique will not be responsible for any inaccuracies in your senior bio. ",
                     records: {},
                     buttonLink: 'http://tnqportal.mit.edu',
                     buttonText: 'Visit the Technique Student Portal'
